20868717e5aa842836334b7f282e1173c38b18f7
Pedro Rincon Avalos <princon04.20@gmail.com>
2023-02-10 01:24:08 -0600

    Use go routine

--- commandline_app/main.go
+++ commandline_app/main.go
@@ -1,11 +1,80 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
 func main() {
-	n := 8
-	_, msg := isPrime(n)
-	fmt.Println(msg)
+	// print a welcome message
+	intro()
+
+	// Crear un canal para indicar cuando el usuario quiere salir
+	doneChan := make(chan bool)
+
+	// Iniciar una goroutine para leer el input del usuario y correr el programa
+	go readUserInput(doneChan)
+
+	// bloquear hasta que doneChan reciba un valor
+	<-doneChan
+
+	// cerramos el canal
+	close(doneChan)
+
+	// despedir el programa
+	fmt.Println("Goodbay!.")
+
+}
+
+func readUserInput(doneChan chan bool) {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		res, done := checkNumbers(scanner)
+
+		if done {
+			doneChan <- true
+			return
+		}
+
+		fmt.Println(res)
+		prompt()
+
+	}
+}
+
+func checkNumbers(scanner *bufio.Scanner) (string, bool) {
+	// leer el input del usuario
+	scanner.Scan()
+
+	// checar si el usuario quiere salir
+	if strings.EqualFold(scanner.Text(), "q") {
+		return "", true
+	}
+
+	// intentamos convertir el input a entero
+	numToCheck, err := strconv.Atoi(scanner.Text())
+	if err != nil {
+		return "Por favor ingresa un numero entero", false
+	}
+
+	_, msg := isPrime(numToCheck)
+	return msg, false
+
+}
+
+func intro()  {
+	fmt.Println("Is it Prime?")
+	fmt.Println("------------")
+	fmt.Println("Introduce un número y te diremos si es un primo o no. Usa 'q' para salir.")
+	prompt()
+}
+
+func prompt() {
+	fmt.Print("-> ")
 }
 
 func isPrime(n int) (bool, string) {
